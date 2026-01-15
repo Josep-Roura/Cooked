eb976173cdbaa687331457c24d16d52f25b67327
Josep Roura Fernandez <joseprouraf@gmail.com>
2026-01-15 09:02:32 +0100

    Delete old Frontend

--- frontend_old/src/components/ThemeInit.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useInitTheme } from "@/lib/store/useThemeStore";
-
-export function ThemeInit() {
-  useInitTheme();
-  return null;
-}
--- frontend_old/src/lib/auth/getUserIdFromRequest.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-
-export function getUserIdFromRequestOrThrow(): string {
-  const userId = cookies().get("cookedai_user_id")?.value;
-  if (!userId) {
-    throw new Error("NO_AUTH");
-  }
-  return userId;
-}
