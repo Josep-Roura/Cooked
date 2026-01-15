8cc8359ed2dad0800e42b375643d93974344170c
Josep Roura Fernandez <joseprouraf@gmail.com>
2026-01-15 09:02:32 +0100

    Delete old Frontend

--- frontend_old/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Para juntar clases Tailwind sin choques
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
